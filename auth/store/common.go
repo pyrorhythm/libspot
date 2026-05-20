cdd1782e4bc398d3d32e7aa2e38072d42dd07c5b
Alexey Pechenin <me@pyrorhythm.dev>
2026-05-20 18:09:28 +0300

    chore: remove keybase

--- auth/store/common.go
+++ auth/store/common.go
@@ -9,7 +9,3 @@ const storeService = "com.pyrorhythm.libspot"
 func Zalando[T any](key string) keychain.Keychainer[T] {
 	return keychain.NewZalandoKeychainer[T](storeService, key)
 }
-
-func Keybase[T any](key string) keychain.Keychainer[T] {
-	return keychain.NewKeybaseKeychainer[T](storeService, key)
-}
--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/go-jose/go-jose/v4 v4.1.4
 	github.com/goccy/go-json v0.10.6
 	github.com/google/uuid v1.6.0
-	github.com/keybase/go-keychain v0.0.1
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
 	github.com/pkg/errors v0.9.1
 	github.com/pyrorhythm/fn v0.7.4
